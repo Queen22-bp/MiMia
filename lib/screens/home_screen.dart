89e231df9ff0182748554820dd3a51a5dd8f9901
Queen22-bp <irbelly2@gmail.com>
2026-03-15 23:44:03 +0200

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,1 +1,46 @@
+import 'package:flutter/material.dart';
+import 'feed_screen.dart';
+import 'messages_screen.dart';
+import 'music_screen.dart';
+import 'videos_screen.dart';
 
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    FeedScreen(),
+    MessagesScreen(),
+    MusicScreen(),
+    VideosScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.purple,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Feed'),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Messages'),
+          BottomNavigationBarItem(icon: Icon(Icons.music_note), label: 'Music'),
+          BottomNavigationBarItem(icon: Icon(Icons.video_library), label: 'Videos'),
+        ],
+      ),
+    );
+  }
+}
